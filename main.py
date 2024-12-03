37af7ce838ac1d357e685eb3f25fee284b02c2cd
BrayanStevenRianoFuentesADSO <128181660+BrayanStevenRianoFuentesADSO@users.noreply.github.com>
2024-12-03 07:50:03 -0500

    temas visto, variables, listas, entrada y salida de datos

--- main.py
+++ main.py
@@ -1,2 +1,22 @@
-print("hello world")
+#variables0
+
+#tipos de datos primitivos
+#
+# string cadena de caracteres
+#char caracter str
+# number enteros int decimales float
+# boolean 
+# #
+name="brayan"
+age=20
+heigth= 1.62
+trainer = false
+
+#conjuntos de datos
+
+hobbies= list("cocinar", "jugar", "dormir", "musica")
+
+#hobbies[2]="despertar"
+hobbies.pop
+
 
